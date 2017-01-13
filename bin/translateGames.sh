80f46639a62fe2fde721100b5ff1f2353ac09ded
battlemage10000 <battlemage10000@gmail.com>
2017-01-13 15:25:41 +1300

    Tidy mass translation script

--- bin/translateGames.sh
+++ bin/translateGames.sh
@@ -1,24 +1,50 @@
 ant jar
-echo montyhall
+echo
+echo Monty Hall mck
 java -jar deploy/MckTranslator.jar -i res/gdlii/MontyHall.gdl -o mh-prover.mck --use-prover
+echo
+echo Monty Hall mck with define
 java -jar deploy/MckTranslator.jar -i res/gdlii/MontyHall.gdl -o mh-prover-define.mck --use-prover --use-define
-mck mh-prover.mck
-mck mh-prover-define.mck
-
-echo kreigtictactoe
+echo
+echo Kreig Tic Tac Toe mck
 java -jar deploy/MckTranslator.jar -i res/gdlii/kriegtictactoe.gdl -o kttt-prover.mck --use-prover
+echo
+echo Kreig Tic Tac Toe mck with define
 java -jar deploy/MckTranslator.jar -i res/gdlii/kriegtictactoe.gdl -o kttt-prover-define.mck --use-prover --use-define
-mck kttt-prover.mck
-mck kttt-prover-define.mck
-
-echo transit
+echo
+echo Transit mck
 java -jar deploy/MckTranslator.jar -i res/gdlii/transit.gdl -o trans-prover.mck --use-prover
+echo
+echo Transit mck with define
 java -jar deploy/MckTranslator.jar -i res/gdlii/transit.gdl -o trans-prover-define.mck --use-prover --use-define
-mck trans-prover.mck
-mck trans-prover-define.mck
-
-echo meier
+echo
+echo "Meier(Mia) mck"
 java -jar deploy/MckTranslator.jar -i res/gdlii/meier.gdl -o meier-prover.mck --use-prover
+echo
+echo "Meier(Mia) mck with define"
 java -jar deploy/MckTranslator.jar -i res/gdlii/meier.gdl -o meier-prover-define.mck --use-prover --use-define
+
+echo
+echo Monty Hall mck
+mck mh-prover.mck
+echo
+echo Monty Hall mck with define
+mck mh-prover-define.mck
+echo
+echo Kreig Tic Tac Toe mck
+mck kttt-prover.mck
+echo
+echo Kreig Tic Tac Toe mck with define
+mck kttt-prover-define.mck
+echo
+echo Transit mck
+mck trans-prover.mck
+echo
+echo Transit mck with define
+mck trans-prover-define.mck
+echo
+echo Meier(Mia) mck
 mck meier-prover.mck
+echo
+echo Meier(Mia) mck with define
 mck meier-prover-define.mck
