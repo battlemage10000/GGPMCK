62685f10256abc0b089cda8703f1d6ad700c6b80
battlemage10000 <battlemage10000@gmail.com>
2017-02-22 18:01:02 +1300

    Minor changes to spec definition

--- src/translator/MckTranslator.java
+++ src/translator/MckTranslator.java
@@ -846,8 +846,9 @@ public class MckTranslator {
 		StringBuilder spec = new StringBuilder();
 
 		// Specification
-		spec.append(System.lineSeparator() + "--spec_obs = AG((terminal" + MckFormat.OR);
+		//spec.append(System.lineSeparator() + "--spec_obs = AG((");
 		for (String role : ATd.keySet()) {
+			spec.append(System.lineSeparator() + "--spec_obs = AG((");
 			for (String move : ATd.get(role)) {
 				spec.append("(legal_" + role + "_" + move + " => Knows " + MckFormat.ROLE_PREFIX + role + " legal_" + role
 						+ "_" + move + ")");
@@ -856,15 +857,15 @@ public class MckTranslator {
 		}
 		spec.delete(spec.length() - 4, spec.length());
 		spec.append("))");
-		spec.append(System.lineSeparator() + "spec_obs = AG(");
+		spec.append(System.lineSeparator() + "--spec_obs = AG(");
 		for (String role : ATd.keySet()) {
-			spec.append("(neg terminal => neg (" + MckFormat.DOES_PREFIX + role + " == " + MckFormat.MOVE_PREFIX + MckFormat.STOP + "_"
-					+ role + "))");
+			spec.append("((" + MckFormat.DOES_PREFIX + role + " == " + MckFormat.MOVE_PREFIX + MckFormat.STOP + "_"
+					+ role + ") => terminal)");
 			spec.append(MckFormat.AND);
 		}
 		spec.delete(spec.length() - 4, spec.length());
 		spec.append(")");
-		spec.append(System.lineSeparator() + "spec_obs = AF terminal");
+		spec.append(System.lineSeparator() + "--spec_obs = AF terminal");
 		spec.append(System.lineSeparator());
 		spec.append(System.lineSeparator());
 
