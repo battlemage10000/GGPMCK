6ee8e6dc41ccaf82819ea140561d98b941f96a2a
battlemage10000 <battlemage10000@gmail.com>
2017-02-24 14:08:27 +1300

    Change compile-gdl script to take an input dir and output dir as args

--- bin/compile-gdl.sh
+++ bin/compile-gdl.sh
@@ -6,12 +6,20 @@ function compileMck() {
 	java -jar deploy/MckTranslator.jar -i $1 -o $2 --use-prover
 }
 
+# $1 directory with input files
+# $2 directory for output files
 PWD=$(pwd)
-cd res/gdl
-for f in *
+echo PWD $PWD
+GAME_DIR=$1
+echo GAME_DIR $GAME_DIR
+shift
+OUT_DIR=$1
+echo OUT_DIR $OUT_DIR
+#cd $GAME_DIR
+for f in $GAME_DIR*
 do
-	cd ../../
-	compileMck "res/gdl/"$f "gdl.out/"$f".mck"
-	cd res/gdl
+	#cd $PWD
+	compileMck $f $OUT_DIR$(basename $f)".mck"
+	#cd $GAME_DIR
 done
-cd PWD
+cd $PWD
